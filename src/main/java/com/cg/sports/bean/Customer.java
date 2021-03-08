ebde0c1b70b39ca0ceedcb70fd9f96fd24e15d18
bhavesh-gulabani <bhaveshgulabani1@gmail.com>
2021-03-08 12:00:44 +0530

    complete customer and address entities

--- src/main/java/com/cg/sports/bean/Customer.java
+++ src/main/java/com/cg/sports/bean/Customer.java
@@ -25,4 +25,60 @@ public class Customer {
 		this.dob = dob;
 		this.address = address;
 	}
+
+	// Getters and Setters
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getContactNo() {
+		return contactNo;
+	}
+
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+
+	public LocalDate getDob() {
+		return dob;
+	}
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	// toString
+	@Override
+	public String toString() {
+		return "Customer [userId=" + userId + ", name=" + name + ", email=" + email + ", contactNo=" + contactNo
+				+ ", dob=" + dob + ", address=" + address + "]";
+	}
 }

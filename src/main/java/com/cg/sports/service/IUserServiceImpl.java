4d6b99b147f34872ed4fe3865747d8e9e59ac9fe
Chinmayi Joshi <79636352+chinickks@users.noreply.github.com>
2021-03-15 08:10:40 +0530

    Added getAllUsers, getUserById and addUser

--- src/main/java/com/cg/sports/service/IUserServiceImpl.java
+++ src/main/java/com/cg/sports/service/IUserServiceImpl.java
@@ -1,28 +1,43 @@
 package com.cg.sports.service;
 
+import java.util.List;
+
 import com.cg.sports.bean.User;
 import com.cg.sports.dao.IUserRepository;
 import com.cg.sports.dao.IUserRepositoryImpl;
 
-
 public class IUserServiceImpl implements IUserService {
+private IUserRepository repo = new IUserRepositoryImpl() ;
+
+@Override
+public User signIn(User user) {
+	return repo.signIn(user);
 	
-	private IUserRepository repo = new IUserRepositoryImpl() ;
+}
 
-	@Override
-	public User signIn(User user) {
-		return repo.signIn(user);
-		
-	}
+@Override
+public User signOut(User user) {
+	return repo.signOut(user);
+}
 
-	@Override
-	public User signOut(User user) {
-		return repo.signOut(user);
-	}
+@Override
+public User changePassword(long id, User user) {
+	return repo.changePassword(id, user);
+}
 
-	@Override
-	public User changePassword(long id, User user) {
-		return repo.changePassword(id, user);
-	}
+@Override
+public List<User> getAllUsers() {
+	return repo.getAllUsers();
+}
+
+@Override
+public User getUserById(long userId) {
+	return repo.getUserById(userId);
+}
+
+@Override
+public User addUser(User user) {
+	return repo.addUser(user);
+}
 
 }

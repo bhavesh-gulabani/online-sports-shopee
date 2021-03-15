190214565b547df9184d937454fa627dc921a9c8
Chinmayi Joshi <79636352+chinickks@users.noreply.github.com>
2021-03-15 08:12:05 +0530

    Updated test cases

--- src/test/java/com/cg/sports/IUserReposImplSpecs.java
+++ src/test/java/com/cg/sports/IUserReposImplSpecs.java
@@ -1,80 +1,136 @@
 package com.cg.sports;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 import com.cg.sports.bean.User;
 import com.cg.sports.dao.IUserRepositoryImpl;
 
-class IUserReposImplSpecs 
-{
+class UserTest {
 
-	User u = new User(1111,"1234","Customer");
-	IUserRepositoryImpl userepo = new IUserRepositoryImpl();
-	
-	@BeforeAll
-	void beforeAllTest()
-	{
-		System.out.println("start testing");
-	}
-	
-	@AfterAll
-	void afterAllTest()
-	{
-		System.out.println("end testing");
-	}
-	
-	@BeforeEach
-	void beforeEachTest()
-	{
-		System.out.println("before testing");
-	}
-	
-	@AfterEach
-	void afterEachTest()
-	{
-		System.out.println("after testing");
-	}
-	
-	
-	@Test
-	void test1() {
-		System.out.println("Test1");
-		User returnUser = userepo.signIn(u);
-		assertNotNull(returnUser);
-        assertEquals(u,returnUser); 
-        System.out.println("Tes1");
-	}
-	
-	
-	
-	
-	@Test
-	void test2() {
-		System.out.println("Test2");
-		User returnUser = userepo.signOut(u);
-		assertEquals(u,returnUser);
-	
-	}
-	
+		User u1 = null;
+		User u2 = new User( " 1234"," customer"," user1234");
+		User u3 = new User("1234","Customer", "abcd");
+		
+		IUserRepositoryImpl userepo = new IUserRepositoryImpl();
+		
+		User user = new User("user123", "1234", "customer");
+		User testResult = userepo.addUser(user);
 
-	@Test
-	void test3() {
-		System.out.println("Test3");
-		User returnUser = userepo.changePassword(1111,u);
-		String password = "abcd";
-		assertEquals(password ,returnUser.getPassword());
+		@BeforeAll
+		void beforeAllTest()
+		{
+			System.out.println("start testing");
+		}
+		
+		@BeforeEach
+		void beforeEachTest()
+		{
+			System.out.println("before testing");
+		}
+		
+		@AfterEach
+		void afterEachTest()
+		{
+			System.out.println("after testing");
+		}
+		
+		@Test
+		
+		void signInNull() {
+			
+			User returnUser = userepo.signIn(u1);
+			assertEquals(null,returnUser); 
+	    }
 
 		
+		@Test
 		
-	}
+		void signInPos() {			
+			User returnUser = userepo.signIn(testResult);
+			assertEquals(testResult,returnUser);
+		}
+		
+		
+		@Test
+		
+		void signInNeg() {
+			User returnUser = userepo.signIn(u3);
+			assertNotEquals(u3, returnUser);
+		}
+
+		@Test
+		
+		void signOutPos() {
+			User returnUser = userepo.signOut(user);
+			assertEquals(user,returnUser);
+		}
+		
+		@Test
+		
+		void signOutNeg() {
+			User returnUser = userepo.signOut(u2);
+			assertNotEquals(u2,returnUser);
+		}
+		
+		@Test
+		
+		void signOutNull() {
+			User returnUser = userepo.signOut(u1);
+			assertNotEquals(u2,returnUser);
+		}
+
+
+		@Test
+		
+		void changePassword() {
+			
+			System.out.println("Test3");
+			User returnUser = userepo.changePassword(user.getUserId(),user);
+			String password = "abcd";
+			assertEquals(password ,returnUser.getPassword());
+
+			
+		}
+		@Test
+		
+		void getUser(){
+			long userId = 512;
+			User expected1 = new User("user123", "1234", "customer");	
+			//expected1.setUserId(512); 
+			 
+			User testResult1 = userepo.getUserById(userId);
+			System.out.println(testResult1);
+			assertAll(
+					() -> assertEquals(expected1.getUsername(),testResult1.getUsername()),
+					() -> assertEquals(expected1.getPassword(),testResult1.getPassword()),
+					() -> assertEquals(expected1.getRole(),testResult1.getRole())
+					);		
+		}
+		
+		
+		@Test
+		void getAllCustomers() {
+			List<User> testResult = userepo.getAllUsers();
+			assertNotNull(testResult);
+		}
+		
+		@AfterAll
+		void afterAllTest()
+		{
+			System.out.println("end testing");
+		}
+
 
 }

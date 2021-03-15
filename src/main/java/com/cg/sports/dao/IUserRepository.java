800e2e9820e0a3fd39851120f49ca2804b97b654
Chinmayi Joshi <79636352+chinickks@users.noreply.github.com>
2021-03-15 08:02:43 +0530

    Update IUserRepository.java
    Added addUser Method

--- src/main/java/com/cg/sports/dao/IUserRepository.java
+++ src/main/java/com/cg/sports/dao/IUserRepository.java
@@ -1,9 +1,15 @@
 package com.cg.sports.dao;
 
+import java.util.List;
+
+
 import com.cg.sports.bean.User;
 
 public interface IUserRepository {
 	public User signIn(User user);
 	public User signOut(User user);
+	public User addUser(User user);
 	public User changePassword(long id, User user);
+	public List<User> getAllUsers();
+	public User getUserById(long custId);
 }
